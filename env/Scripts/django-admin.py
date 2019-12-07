28217f9cf4fde3df682519bd72584f9bfa7acf01
Amanda Reges <reges.amanda1@gmail.com>
2019-12-07 19:19:37 -0200

    estrutura djando

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\alex\desktop\s3-18-exercicio-notify-django\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ projeto1ex/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Projeto1ExConfig(AppConfig):
+    name = 'projeto1ex'
